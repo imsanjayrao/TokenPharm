ac73ccb04c330f356d8cc1d96e4673b88a6421a0
Sanjay Rao <code@imsanjayrao.eu.org>
2022-11-07 13:43:45 +0530

    update smart contract

--- contracts/TokenPharm.sol
+++ contracts/TokenPharm.sol
@@ -4,8 +4,30 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenPharm is ERC20 {
-    uint constant _initial_supply = 1000000 * (10**18);
+    uint constant _initial_supply = 0 * (10**18);
+    address owner;
     constructor() ERC20("TokenPharm", "TP") {
+        owner = msg.sender;
         _mint(msg.sender, _initial_supply);
+    }
+    address[] manufacturersList;
+
+    function verifyManufacturer() public view returns (bool){
+        for (uint i=0; i<manufacturersList.length; i++){
+            if (manufacturersList[i] == msg.sender){
+                return true;
+            }
+        }
+        return false;
+    }
+    function addManufacturer(address manAdd) public {
+        require (msg.sender == owner);
+        manufacturersList.push(manAdd);
+    }
+
+
+    function manufacturerMinter() public {
+        require(verifyManufacturer());
+        _mint(msg.sender, 1000);
     }
 }
